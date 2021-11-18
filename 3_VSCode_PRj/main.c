a9ca0130b0acfcf0ab06e9cb7d22c85a9c448734
Andrea D'Aguanno <daguannoandrea@outlook.com>
2021-11-19 00:05:39 +0100

    cc2

--- 3_VSCode_PRj/main.c
+++ 3_VSCode_PRj/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-int main(){
-    printf("Ciao ciao ciao");
+int main()
+{
+    bool check = false;
+    //_Bool check = true; 
+
+    if (check)
+    {
+
+        printf("Ciao true");
+    }
+    else
+    {
+
+        printf("Ciao false");
+    }
     return 0;
 }
